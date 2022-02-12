8ab326144ff865ae2362817c51b75d91c421ffca
ktogias(inspiron) <info@ktogias.gr>
2022-02-12 23:26:15 +0200

    Updated bin scripts and removed container name

--- bin/lib/command/logs.sh
+++ bin/lib/command/logs.sh
@@ -2,5 +2,4 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source $PARENT_PATH/../lib.sh
 
-source $PARENT_PATH/build.sh
-eval "$COMPOSE_COMMAND restart $@"
+eval "$COMPOSE_COMMAND logs $@"
